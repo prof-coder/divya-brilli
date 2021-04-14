220b248bcd3ac6e36793632a231669f92deb0eb6
Admin <profcoder810@gmail.com>
2021-04-14 11:43:18 -0400

    Inited: Project

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=dadce9b9-0cc1-48a4-b42d-2ce656a97e3b';document.getElementsByTagName('head')[0].appendChild(script);
